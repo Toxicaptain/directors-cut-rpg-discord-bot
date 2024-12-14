41987a9c1d2772d5567acd4100343f7e32d212cb
Mirko Froehlich <682+digitalhobbit@users.noreply.github.com>
2024-12-13 21:36:05 -0700

    encode chosen dice set in button custom ids so they still work even after the dice set is changed

--- bot/controller.py
+++ bot/controller.py
@@ -4,6 +4,7 @@ This module contains classes for handling commands for the Outgunned bot,
 including generating the view for the roll command with buttons for rerolling,
 free rerolling, and going all in.
 """
+from abc import ABC, abstractmethod
 import re
 import discord
 from bot.dice import DiceSet
@@ -48,16 +49,16 @@ class RollController:
         well as a view containing buttons for rerolling, free rerolling,
         and going all in.
         """
+        dice_set = dice_set_for_interaction(interaction)
         roller = Roller(num_dice=num_dice)
         roller.roll()
         view = RollView(
             user_id=interaction.user.id,
+            dice_set=dice_set,
             can_reroll=roller.roll_history.can_reroll(),
             can_free_reroll=roller.roll_history.can_free_reroll(),
             can_go_all_in=roller.roll_history.can_go_all_in())
-        content = MessageGenerator(
-            dice_set_for_interaction(interaction)
-        ).generate_roll_message(roller.roll_history)
+        content = MessageGenerator(dice_set).generate_roll_message(roller.roll_history)
         embed = discord.Embed(description=content, color=EMBED_COLOR)
         await interaction.response.send_message(embed=embed, view=view)    
 
@@ -100,121 +101,122 @@ class RollView(discord.ui.View):
 
     Contains buttons for rerolling, free rerolling, and going all in.
     """
-    def __init__(self, user_id: int, can_reroll: bool, can_free_reroll: bool, can_go_all_in: bool):
+    def __init__(self, user_id: int, dice_set: DiceSet, can_reroll: bool, can_free_reroll: bool, can_go_all_in: bool):
         super().__init__(timeout=None)
         if can_reroll:
-            self.add_item(DynamicRerollButton(user_id))
+            self.add_item(DynamicRerollButton(user_id, dice_set))
         if can_free_reroll:
-            self.add_item(DynamicFreeRerollButton(user_id))
+            self.add_item(DynamicFreeRerollButton(user_id, dice_set))
         if can_go_all_in:
-            self.add_item(DynamicAllInButton(user_id))
+            self.add_item(DynamicAllInButton(user_id, dice_set))
+
+
+class AbstractDynamicButton(discord.ui.DynamicItem[discord.ui.Button], ABC, template=r''):
+    """An abstract class for dynamic buttons.
+    
+    We're wrapping the reroll buttons in DynamicItems so they continue to work
+    after the bot restarts. And we're extracting the common functionality into
+    this abstract class.
+
+    Subclasses must implement the callback method.
+    """
+    def __init__(self, user_id: int, dice_set: DiceSet, label: str, style: discord.ButtonStyle, custom_id: str):
+        self.user_id = user_id
+        self.dice_set = dice_set
+        super().__init__(
+            discord.ui.Button(label=label, style=style, custom_id=custom_id))
 
     @classmethod
-    async def _update_message(cls, interaction: discord.Interaction, roll_history: RollHistory, dice_set: DiceSet):
+    async def from_custom_id(cls, interaction: discord.Interaction, item: discord.ui.Button, match: re.Match[str], /):
+        user_id = int(match['user_id'])
+        dice_set = DiceSet(match['dice_set'])
+        return cls(user_id, dice_set)
+
+    @abstractmethod
+    async def callback(self, interaction: discord.Interaction):
+        pass
+
+    async def interaction_check(self, interaction):
+        if interaction.user.id == self.user_id:
+            return True
+        else:
+            await interaction.response.send_message('You cannot re-roll someone else\'s roll.', ephemeral=True)
+            return False
+
+    async def _update_message(self, interaction: discord.Interaction, roll_history: RollHistory):
         updated_view = RollView(
             user_id=interaction.user.id,
+            dice_set=self.dice_set,
             can_reroll=roll_history.can_reroll(),
             can_free_reroll=roll_history.can_free_reroll(),
             can_go_all_in=roll_history.can_go_all_in())
 
-        message = MessageGenerator(dice_set).generate_roll_message(roll_history)
+        message = MessageGenerator(self.dice_set).generate_roll_message(roll_history)
         embed = discord.Embed(description=message, color=EMBED_COLOR)
         try:
             await interaction.response.edit_message(embed=embed, view=updated_view)
         except Exception as e:
             print(f"Failed to update message: {e}")
 
-# NB: We're wrapping the reroll buttons in DynamicItems so they continue to work after the bot restarts.
 
-class DynamicRerollButton(discord.ui.DynamicItem[discord.ui.Button], template=r'roll:reroll:user:(?P<user_id>[0-9]+)'):
-    def __init__(self, user_id: int):
+class DynamicRerollButton(AbstractDynamicButton, template=r'roll:reroll:user:(?P<user_id>[0-9]+):dice_set:(?P<dice_set>\w+)'):
+    def __init__(self, user_id: int, dice_set: DiceSet):
         self.user_id = user_id
+        self.dice_set = dice_set
         super().__init__(
-            discord.ui.Button(
-                label='Re-roll',
-                style=discord.ButtonStyle.green,
-                custom_id=f'roll:reroll:user:{user_id}'))
+            user_id=user_id,
+            dice_set=dice_set,
+            label='Re-roll',
+            style=discord.ButtonStyle.green,
+            custom_id=f'roll:reroll:user:{user_id}:dice_set:{dice_set.value}')
 
     async def callback(self, interaction: discord.Interaction):
         print('Rerolling...')
-        dice_set = dice_set_for_interaction(interaction)
-        roll_history = MessageParser(interaction, dice_set).roll_history
+        roll_history = MessageParser(interaction, self.dice_set).roll_history
         if not roll_history.can_reroll():
             raise RuntimeError('Cannot perform reroll')
         Roller(roll_history=roll_history).reroll()
 
-        await RollView._update_message(interaction, roll_history, dice_set)
+        await self._update_message(interaction, roll_history)
 
-    async def interaction_check(self, interaction):
-        if interaction.user.id == self.user_id:
-            return True
-        else:
-            await interaction.response.send_message('You cannot re-roll someone else\'s roll.', ephemeral=True)
-            return False
 
-    @classmethod
-    async def from_custom_id(cls, interaction: discord.Interaction, item: discord.ui.Button, match: re.Match[str], /):
-        user_id = int(match['user_id'])
-        return cls(user_id)
-    
-class DynamicFreeRerollButton(discord.ui.DynamicItem[discord.ui.Button], template=r'roll:free_reroll:user:(?P<user_id>[0-9]+)'):
-    def __init__(self, user_id: int):
+class DynamicFreeRerollButton(AbstractDynamicButton, template=r'roll:free_reroll:user:(?P<user_id>[0-9]+):dice_set:(?P<dice_set>\w+)'):
+    def __init__(self, user_id: int, dice_set: DiceSet):
         self.user_id = user_id
+        self.dice_set = dice_set
         super().__init__(
-            discord.ui.Button(
-                label='Free Re-roll',
-                style=discord.ButtonStyle.blurple,
-                custom_id=f'roll:free_reroll:user:{user_id}'))
+            user_id=user_id,
+            dice_set=dice_set,
+            label='Free Re-roll',
+            style=discord.ButtonStyle.blurple,
+            custom_id=f'roll:free_reroll:user:{user_id}:dice_set:{dice_set.value}')
 
     async def callback(self, interaction: discord.Interaction):
         print('Free rerolling...')
-        dice_set = dice_set_for_interaction(interaction)
-        roll_history = MessageParser(interaction, dice_set).roll_history
+        roll_history = MessageParser(interaction, self.dice_set).roll_history
         if not roll_history.can_free_reroll():
             raise RuntimeError('Cannot perform free reroll')
         Roller(roll_history=roll_history).free_reroll()
 
-        await RollView._update_message(interaction, roll_history, dice_set)
+        await self._update_message(interaction, roll_history)
 
-    async def interaction_check(self, interaction):
-        if interaction.user.id == self.user_id:
-            return True
-        else:
-            await interaction.response.send_message('You cannot re-roll someone else\'s roll.', ephemeral=True)
-            return False
 
-    @classmethod
-    async def from_custom_id(cls, interaction: discord.Interaction, item: discord.ui.Button, match: re.Match[str], /):
-        user_id = int(match['user_id'])
-        return cls(user_id)
-
-class DynamicAllInButton(discord.ui.DynamicItem[discord.ui.Button], template=r'roll:all_in:user:(?P<user_id>[0-9]+)'):
-    def __init__(self, user_id: int):
+class DynamicAllInButton(AbstractDynamicButton, template=r'roll:all_in:user:(?P<user_id>[0-9]+):dice_set:(?P<dice_set>\w+)'):
+    def __init__(self, user_id: int, dice_set: DiceSet):
         self.user_id = user_id
+        self.dice_set = dice_set
         super().__init__(
-            discord.ui.Button(
-                label='All In',
-                style=discord.ButtonStyle.red,
-                custom_id=f'roll:all_in:user:{user_id}'))
+            user_id=user_id,
+            dice_set=dice_set,
+            label='All In',
+            style=discord.ButtonStyle.red,
+            custom_id=f'roll:all_in:user:{user_id}:dice_set:{dice_set.value}')
 
     async def callback(self, interaction: discord.Interaction):
         print('All in...')
-        dice_set = dice_set_for_interaction(interaction)
-        roll_history = MessageParser(interaction, dice_set).roll_history
+        roll_history = MessageParser(interaction, self.dice_set).roll_history
         if not roll_history.can_go_all_in():
             raise RuntimeError('Cannot go all in')
         Roller(roll_history=roll_history).all_in()
 
-        await RollView._update_message(interaction, roll_history, dice_set)
-
-    async def interaction_check(self, interaction):
-        if interaction.user.id == self.user_id:
-            return True
-        else:
-            await interaction.response.send_message('You cannot re-roll someone else\'s roll.', ephemeral=True)
-            return False
-
-    @classmethod
-    async def from_custom_id(cls, interaction: discord.Interaction, item: discord.ui.Button, match: re.Match[str], /):
-        user_id = int(match['user_id'])
-        return cls(user_id)
+        await self._update_message(interaction, roll_history)
